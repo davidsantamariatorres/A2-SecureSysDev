a9c465eac563a3665fc0073bd0a6f11017aa1b8f
deivit21 <david29.st@gmail.com>
2024-04-11 18:06:15 +0100

    Trying to import aes python

--- tests/test1.py
+++ tests/test1.py
@@ -4,7 +4,9 @@ import sys
 
 #module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'aes'))
 #sys.path.append(module_path)
-from ..aes import encrypt, decrypt
+#from ..aes import encrypt, decrypt
+sys.path.insert(1, 'aes/aes.py')
+from aes import encrypt, decrypt
 
 # Load the shared object file
 rijndael = ctypes.CDLL('./rijndael.so')
